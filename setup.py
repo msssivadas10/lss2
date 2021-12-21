1eb16523e85abbfc93aa5e7e1c2e3517f554d74d
M. S. Suryan Sivadas <95347340+msssivadas10@users.noreply.github.com>
2021-12-21 14:31:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+    name        = "LSS",
+    version     = "2.0",
+    description = "A python module to do computations related to large-scale structure formation in cosmology",
+    author      = "M. S. Suryan Sivadas",
+    url         = "https://github.com/msssivadas10/lss2.git",
+    py_modules  = ["lss2"],
+)
